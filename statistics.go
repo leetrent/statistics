9c4a1c568f56bf4588d7bdc4e17feb2e4a788bd1
LeeTrent <Lee.Trent.1@gmail.com>
2017-03-19 14:50:58 -0500

    Initial commit, adds statistics.go

---
+++ statistics.go
@@ -0,0 +1,38 @@
+package statistics
+
+import "math"
+
+func CalcMean(data []float64) float64 {
+	var sum float64
+	for _, val := range(data) {
+		sum += val
+	}
+	return sum / float64(len(data))
+}
+
+func CalcVariance(data []float64) float64 {
+
+	return CalcVarianceUsingMean(CalcMean(data), data)
+}
+
+func CalcStandardDeviation( data[] float64) float64 {
+	return math.Sqrt(CalcVariance(data))
+}
+
+func CalcVarianceUsingMean(mean float64, data []float64) float64 {
+	sqDevFromMean := make([]float64, len(data)) // squared deviation from mean
+
+	for _, val := range(data) {
+		valMinusMean := val - mean
+		sqDevFromMean = append(sqDevFromMean, valMinusMean * valMinusMean)
+	}
+
+	variance := CalcMean(sqDevFromMean)
+	return variance
+}
+
+func CalcStandardDeviationUsingVariance(variance float64) float64 {
+	return math.Sqrt(variance)
+}
+
+
