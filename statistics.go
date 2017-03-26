29bb4ec62a421defbf452a8b48dd013d0c8e46b8
LeeTrent <Lee.Trent.1@gmail.com>
2017-03-26 09:56:24 -0500

    Adds CalcStandardError function

--- statistics.go
+++ statistics.go
@@ -1,10 +1,13 @@
 package statistics
 
-import "math"
+import (
+	"errors"
+	"math"
+)
 
 func CalcMean(data []float64) float64 {
 	var sum float64
-	for _, val := range(data) {
+	for _, val := range data {
 		sum += val
 	}
 	return sum / float64(len(data))
@@ -15,16 +18,16 @@ func CalcVariance(data []float64) float64 {
 	return CalcVarianceUsingMean(CalcMean(data), data)
 }
 
-func CalcStandardDeviation( data[] float64) float64 {
+func CalcStandardDeviation(data []float64) float64 {
 	return math.Sqrt(CalcVariance(data))
 }
 
 func CalcVarianceUsingMean(mean float64, data []float64) float64 {
 	var sqDevFromMean []float64 // squared deviation from mean
 
-	for _, val := range(data) {
+	for _, val := range data {
 		valMinusMean := val - mean
-		sqDevFromMean = append(sqDevFromMean, valMinusMean * valMinusMean)
+		sqDevFromMean = append(sqDevFromMean, valMinusMean*valMinusMean)
 	}
 
 	variance := CalcMean(sqDevFromMean)
@@ -35,4 +38,9 @@ func CalcStandardDeviationUsingVariance(variance float64) float64 {
 	return math.Sqrt(variance)
 }
 
-
+func CalcStandardError(popStdDev float64, sampleSize int64) (float64, error) {
+	if sampleSize == 0 {
+		return 0.0, errors.New("sampleSize cannot by zero")
+	}
+	return popStdDev / math.Sqrt(float64(sampleSize)), nil
+}
